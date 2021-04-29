d42092174e1602622dbc92a8ba0e3123f392ae0b
kpcn <channyein.k@gmail.com>
2021-04-30 00:16:39 +0630

    Setup React-Materialize ui

--- setupJest.js
+++ setupJest.js
@@ -1,4 +1,7 @@
 import Enzyme from "enzyme";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 
+// eslint-disable-next-line no-undef
+global.M = require("materialize-css");
+
 Enzyme.configure({ adapter: new Adapter() });
