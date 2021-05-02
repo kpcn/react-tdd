e712263f79e1096bacfcc85da5595926fe8e8d8e
kpcn <channyein.k@gmail.com>
2021-05-03 00:13:42 +0630

    update test case name in e2e test

--- cypress/integration/adding_a_restaurant.spec.js
+++ cypress/integration/adding_a_restaurant.spec.js
@@ -1,5 +1,19 @@
 describe("Adding a restaurant", () => {
-  it("visit the site properly", () => {
+  it("display the restaurant name in the list", () => {
+    const name = "Yummy Yummy";
+
     cy.visit("http://localhost:1234");
+
+    cy.get('[data-test="newRestaurantName"]').should("not.be.visible");
+
+    cy.get('[data-test="addRestaurantButton"]').click();
+
+    cy.get('[data-test="newRestaurantName"]').type(name);
+
+    cy.get('[data-test="saveNewRestaurantButton"]').click();
+
+    cy.get('[data-test="newRestaurantName"]').should("not.be.visible");
+
+    cy.contains(name);
   });
 });
